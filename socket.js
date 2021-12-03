28259d24cc94b5b27bc67beb9438c5a240947398
Leonard Pepa <leopepp4@gmail.com>
2021-12-03 03:22:54 +0200

    trying to fix bug with socket io and port

--- socket.js
+++ socket.js
@@ -4,6 +4,8 @@ const jwt = require("jsonwebtoken");
 
 const io = new Server(server, {
   cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
