f3f0ef039bdc5ae96ce0472a8bf34bbc128077ee
Leonard Pepa <leopepp4@gmail.com>
2021-12-26 03:14:55 +0200

    session saveUninitialized false

--- server.config.js
+++ server.config.js
@@ -18,7 +18,7 @@ app.use(
   session({
     secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     sameSite: true,
     cookie: {
       httpOnly: true,
