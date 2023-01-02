b98e2361809cf129838dc25345a7f7ccb7f1147a
Leonard Pepa <75704905+Leonardpepa@users.noreply.github.com>
2023-01-02 02:33:48 +0200

    session configuration

--- server.config.js
+++ server.config.js
@@ -18,7 +18,7 @@ app.use(
   session({
     secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
       httpOnly: true,
     },
